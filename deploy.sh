a8178ee2e7efd42b53363674faf5be1b4a08af10
Ben Taitelbaum <ben@coshx.com>
2015-01-07 02:19:01 -0500

    tags images as :latest as well

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p "${DOCKER_PASS}"
 for name in `docker ps -f status=running | awk -F '  +' '{name=$7; sub(/_[0-9]+$/, "", name); print name}'`; do
     SHA=${CIRCLE_SHA1:0:7}
-    IMG=${CIRCLE_PROJECT_USERNAME}/$name:$SHA
+    IMG=${CIRCLE_PROJECT_USERNAME}/$name
 
-    docker tag $name:latest $IMG && docker push $IMG || echo "Failed to push $name - do you want to build this image and have a docker hub repository set up for it?" >&2
+    docker tag $name:latest $IMG:latest && docker tag $name:latest $IMG:$SHA && docker push $IMG || echo "Failed to push $name - do you want to build this image and have a docker hub repository set up for it?" >&2
 done
